2f7ef067426b2f9fcac9b66da36e274d9cbc884c
Wyatt French <wyattfrench@MacBook-Pro-7.local>
2023-03-10 13:43:41 -0600

    Updated FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,14 @@
 for num in range(1,101):
+    
     string = ""
+   
     if num % 3 == 0:
         string = string + "Fizz"
-    if num % 4 == 0:
+    
+    if num % 5 == 0:
         string = string + "Buzz"
-    if num % 4 != 0 and num % 3 != 0:
+    
+    if num % 5 != 0 and num % 3 != 0:
         string = string + str(num)
+    
     print(string)
